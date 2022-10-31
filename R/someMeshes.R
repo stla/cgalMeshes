3bdb53a53456d76243b022c5f59ad5a7d30fcbfc
stla <laurent_step@yahoo.fr>
2022-10-31 13:33:47 +0100

    torus mesh & cyclide mesh

--- R/someMeshes.R
+++ R/someMeshes.R
@@ -24,3 +24,171 @@ sphereMesh <- function(x = 0, y = 0, z = 0, r = 1, iterations = 3L) {
   sphere[["normals"]] <- vs
   sphere
 }
+
+#' @title Torus mesh
+#' @description Triangle mesh of a torus.
+#'
+#' @param R,r major and minor radii, positive numbers
+#' @param nu,nv numbers of subdivisions, integers (at least 3)
+#'
+#' @return A triangle \strong{rgl} mesh (class \code{mesh3d}).
+#' @export
+#'
+#' @importFrom rgl tmesh3d
+#'
+#' @examples
+#' library(cgalMeshes)
+#' library(rgl)
+#' mesh <- torusMesh(R = 3, r = 1)
+#' open3d(windowRect = 50 + c(0, 0, 512, 512))
+#' view3d(0, 0, zoom = 0.75)
+#' shade3d(mesh, color = "green")
+#' wire3d(mesh)
+torusMesh <- function(R, r, nu = 50, nv = 30){
+  stopifnot(isPositiveNumber(R), isPositiveNumber(r))
+  stopifnot(R > r)
+  stopifnot(nu >= 3, nv >= 3)
+  nu <- as.integer(nu)
+  nv <- as.integer(nv)
+  nunv <- nu*nv
+  vs      <- matrix(NA_real_, nrow = 3L, ncol = nunv)
+  normals <- matrix(NA_real_, nrow = nunv, ncol = 3L)
+  tris1   <- matrix(NA_integer_, nrow = 3L, ncol = nunv)
+  tris2   <- matrix(NA_integer_, nrow = 3L, ncol = nunv)
+  u_ <- seq(0, 2*pi, length.out = nu + 1L)[-1L]
+  cosu_ <- cos(u_)
+  sinu_ <- sin(u_)
+  v_ <- seq(0, 2*pi, length.out = nv + 1L)[-1L]
+  cosv_ <- cos(v_)
+  sinv_ <- sin(v_)
+  Rrcosv_ <- R + r*cosv_
+  rsinv_ <- r*sinv_
+  jp1_ <- c(2L:nv, 1L)
+  j_ <- 1L:nv
+  for(i in 1L:(nu-1L)){
+    i_nv <- i*nv
+    k1 <- i_nv - nv
+    rg <- (k1 + 1L):i_nv
+    cosu_i <- cosu_[i]
+    sinu_i <- sinu_[i]
+    vs[, rg] <- rbind(
+      cosu_i * Rrcosv_,
+      sinu_i * Rrcosv_,
+      rsinv_
+    )
+    normals[rg, ] <- cbind(
+      cosu_i * cosv_,
+      sinu_i * cosv_,
+      sinv_
+    )
+    k_ <- k1 + j_
+    l_ <- k1 + jp1_
+    m_ <- i_nv + j_
+    tris1[, k_] <- rbind(m_, l_, k_)
+    tris2[, k_] <- rbind(m_, i_nv + jp1_, l_)
+  }
+  i_nv <- nunv
+  k1 <- i_nv - nv
+  rg <- (k1 + 1L):i_nv
+  vs[, rg] <- rbind(
+    Rrcosv_,
+    0,
+    rsinv_
+  )
+  normals[rg, ] <- cbind(
+    cosv_,
+    0,
+    sinv_
+  )
+  l_ <- k1 + jp1_
+  k_ <- k1 + j_
+  tris1[, k_] <- rbind(j_, l_, k_)
+  tris2[, k_] <- rbind(j_, jp1_, l_)
+  tmesh3d(
+    vertices = vs,
+    indices = cbind(tris1, tris2),
+    normals = normals,
+    homogeneous = FALSE
+  )
+}
+
+#' @title Cyclide mesh
+#' @description Triangle mesh of a Dupin cyclide.
+#'
+#' @param a,c,mu cyclide parameters, positive numbers such that
+#'   \code{c < mu < a}
+#' @param nu,nv numbers of subdivisions, integers (at least 3)
+#'
+#' @return A triangle \strong{rgl} mesh (class \code{mesh3d}).
+#'
+#' @details The Dupin cyclide in the plane \emph{z=0}:
+#' \if{html}{
+#'   \figure{cyclide.png}{options: style="max-width:75\%;" alt="cyclide"}
+#' }
+#' \if{latex}{
+#'   \out{\begin{center}}\figure{cyclide.png}\out{\end{center}}
+#' }
+#'
+#' @export
+#'
+#' @importFrom rgl tmesh3d
+#'
+#' @examples
+#' library(cgalMeshes)
+#' library(rgl)
+#' mesh <- cyclideMesh(a = 97, c = 32, mu = 57)
+#' sphere <- sphereMesh(x = 32, y = 0, z = 0, r = 40)
+#' open3d(windowRect = 50 + c(0, 0, 512, 512))
+#' view3d(0, 0, zoom = 0.75)
+#' shade3d(mesh, color = "chartreuse")
+#' wire3d(mesh)
+#' shade3d(sphere, color = "red")
+#' wire3d(sphere)
+cyclideMesh <- function(a, c, mu, nu = 90L, nv = 40L){
+  stopifnot(c > 0, a > mu, mu > c)
+  stopifnot(nu >= 3, nv >= 3)
+  nu <- as.integer(nu)
+  nv <- as.integer(nv)
+  vertices <- matrix(NA_real_, nrow = 3L, ncol = nu*nv)
+  normals  <- matrix(NA_real_, nrow = nu*nv, ncol = 3L)
+  b2 <- a * a - c * c;
+  bb <- sqrt(b2 * (mu * mu - c * c))
+  omega <- (a * mu + bb) / c
+  Omega0 <- c(omega, 0, 0)
+  inversion <- function(M) {
+    OmegaM <- M - Omega0
+    k <- c(crossprod(OmegaM))
+    OmegaM / k + Omega0
+  }
+  h <- (c * c) / ((a - c) * (mu - c) + bb)
+  r <- (h * (mu - c)) / ((a + c) * (mu - c) + bb)
+  R <- (h * (a - c)) / ((a - c) * (mu + c) + bb)
+  bb2 <- b2 * (mu * mu - c * c)
+  denb1 <- c * (a*c - mu*c + c*c - a*mu - bb)
+  b1 <- (a*mu*(c-mu)*(a+c) - bb2 + c*c + bb*(c*(a-mu+c) - 2*a*mu))/denb1
+  denb2 <- c * (a*c - mu*c - c*c + a*mu + bb)
+  b2 <- (a*mu*(c+mu)*(a-c) + bb2 - c*c + bb*(c*(a-mu-c) + 2*a*mu))/denb2
+  omegaT <- (b1 + b2)/2
+  OmegaT <- c(omegaT, 0, 0)
+  tormesh <- torusMesh(R, r, nu, nv)
+  rtnormals <- r * tormesh[["normals"]][1L:3L, ]
+  xvertices <- tormesh[["vb"]][1L:3L, ] + OmegaT
+  for(i in 1L:nu){
+    k0 <- i * nv - nv
+    for(j in 1L:nv){
+      k <- k0 + j
+      rtnormal <- rtnormals[, k]
+      xvertex <- xvertices[, k]
+      vertex <- inversion(xvertex)
+      vertices[, k] <- vertex
+      foo <- vertex - inversion(rtnormal + xvertex)
+      normals[k, ] <- foo / sqrt(c(crossprod(foo)))
+    }
+  }
+  tmesh3d(
+    vertices    = vertices,
+    indices     = tormesh[["it"]],
+    normals     = normals,
+    homogeneous = FALSE
+  )
+}
