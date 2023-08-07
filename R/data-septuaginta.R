b4db4dad62aa98709138020d0acd67d7bed0c0ce
stla <laurent_step@yahoo.fr>
2023-08-07 11:05:09 +0200

    septuaginta

---
+++ R/data-septuaginta.R
@@ -0,0 +1,7 @@
+#' @title Mesh of the septuaginta (Da Vinci's 72-sided sphere)
+#'
+#' @description A \code{mesh3d} object.
+#'  There are some triangle faces and some quad faces.
+#'
+#' @format A \strong{rgl} mesh (class \code{mesh3d}).
+"septuaginta"
